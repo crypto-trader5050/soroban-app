e7d6fac8c1d7af2340b20d399c96555543134765
crypto-trader5050 <eradetective_no5050@outlook.com>
2026-08-25 00:11:50 +0900

    PWA対応を追加

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+// そろばんトレーニング
+// PWA Service Worker
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
